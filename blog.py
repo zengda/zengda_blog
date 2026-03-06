bc810f556851bc00459b207f434cc6bcd79e9d74
zengda <308831499@qq.com>
2026-03-07 00:05:45 +0800

    增加新建文章功能

--- blog.py
+++ blog.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, flash, redirect
 import sqlite3
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your secret key'
 
 def get_db_connection():
     conn = sqlite3.connect('database.db')
@@ -15,7 +16,26 @@ def index():
     conn.close()
     return render_template('index.html', posts=posts)
 
+@app.route('/create', methods=('GET', 'POST'))
+def create():
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
 
+        if not title:
+            flash('标题不能为空！')
+        elif not content:
+            flash('内容不能为空！')
+        else:
+            conn = get_db_connection()
+            conn.execute('INSERT INTO posts (title, content) VALUES (?, ?)',
+                         (title, content))
+            conn.commit()
+            conn.close()
+            flash('文章发布成功！')
+            return redirect(url_for('index'))
+
+    return render_template('create.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
